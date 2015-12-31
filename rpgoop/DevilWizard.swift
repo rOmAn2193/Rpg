3081ef7a02505153dfb8a29da7d63bef98883bff
rOmAn2193 <roman_puzey@aol.com>
2015-12-31 01:06:43 -0700

    first commit

---
+++ rpgoop/DevilWizard.swift
@@ -0,0 +1,9 @@
+//
+//  DevilWizard.swift
+//  rpgoop
+//
+//  Created by Roman on 12/30/15.
+//  Copyright © 2015 Roman Puzey. All rights reserved.
+//
+
+import Foundation
---
+++ rpgoop/Enemy.swift
@@ -0,0 +1,9 @@
+//
+//  Enemy.swift
+//  rpgoop
+//
+//  Created by Roman on 12/30/15.
+//  Copyright © 2015 Roman Puzey. All rights reserved.
+//
+
+import Foundation
